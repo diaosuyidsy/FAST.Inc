f6bef2002ef2a4620fa9532da0376d47d1dd8d3e
Antonio Bountouvas <abountouvas@gmail.com>
2018-01-28 11:14:28 -0500

    Added music room, modified door script...

--- Game/FAST/Assets/Scripts/doorControl.cs
+++ Game/FAST/Assets/Scripts/doorControl.cs
@@ -10,10 +10,14 @@ public class doorControl : MonoBehaviour
 	int codeEntered = 0;
 	bool startLifting = false;
 	float liftTimer = 2f;
+	AudioSource audio;
+	BoxCollider2D coll;
 
 	void Start ()
 	{
 		CodeEntered = new bool[maxCodeNum];
+		audio = GetComponent<AudioSource> ();
+		coll = GetComponent<BoxCollider2D> ();
 	}
 
 	public void codeEnterCorrectly (int num)
@@ -28,6 +32,8 @@ public class doorControl : MonoBehaviour
 		if (CodeEntered.All (code => code)) {
 			Debug.Log ("Open SESAME");
 			GetComponent<Animator> ().SetBool ("DoorOpen", true);
+			audio.Play ();
+			coll.enabled = !coll.enabled;
 		}
 	}
 }
