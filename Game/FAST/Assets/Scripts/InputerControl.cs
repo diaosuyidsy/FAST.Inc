1b44fa66c28e3b0eb00f70e009e2a3ab26b0f6c6
Antonio Bountouvas <abountouvas@gmail.com>
2018-01-28 11:35:02 -0500

    Fixed doors and layers

--- Game/FAST/Assets/Scripts/InputerControl.cs
+++ Game/FAST/Assets/Scripts/InputerControl.cs
@@ -9,6 +9,7 @@ public class InputerControl : MonoBehaviour
 	public int CorrectNum = 3;
 	public int serialNumber;
 	public GameObject InputObjectFor;
+	public GameObject InputObjectForTwo;
 	private bool canType = false;
 	private bool solved = false;
 
@@ -26,6 +27,9 @@ public class InputerControl : MonoBehaviour
 					InputterText.text = answer.ToString ();
 					// Then tell the door the player got it
 					InputObjectFor.GetComponent<doorControl> ().codeEnterCorrectly (serialNumber);
+					if (InputObjectForTwo != null) {
+						InputObjectForTwo.GetComponent<doorControl> ().codeEnterCorrectly (serialNumber);
+					}
 					// Disable the canType so player cannot repeatly enter numbers
 					canType = false;
 					// Marked it as solved
